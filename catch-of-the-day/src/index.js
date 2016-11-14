f545e8a29556b7275a749aad1dbe4ecaedf45b78
Christian Acuña <cacuna0828@gmail.com>
2016-11-13 18:39:05 -0800

    import css to index.js

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,6 +2,8 @@
 /*jshint esversion: 6 */
 import React from 'react';
 import { render } from 'react-dom';
+import './css/style.css';
+
 import StorePicker from './components/StorePicker';
 
 render(<StorePicker/>, document.querySelector('#main'));
